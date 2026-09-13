a4df65d48d7a33628c668718f47eb3c1a25ef51f
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2026-09-13 01:18:58 -0500

    Format files to pass CI

--- cmd/cache-server/main.go
+++ cmd/cache-server/main.go
@@ -1,11 +1,12 @@
 package main
+
 import (
 	"fmt"
 
-    "github.com/ajit-66549/distributed-cache/internal/config"
+	"github.com/ajit-66549/distributed-cache/internal/config"
 )
 
 func main() {
 	cfg := config.Load()
-	fmt.Printf("Distributed cache server is starting...%s\n", cfg.Port,)
+	fmt.Printf("Distributed cache server is starting...%s\n", cfg.Port)
 }
